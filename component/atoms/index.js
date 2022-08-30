08abb72e7f49f308ff49cb5a2ffa25c7708a25b0
Pierre-Alexis Noblet <98712114+P-NBLT@users.noreply.github.com>
2022-08-30 10:01:46 +0200

    typo mistake folder NavBar

--- component/atoms/index.js
+++ component/atoms/index.js
@@ -1,7 +1,7 @@
 export { default as Title } from "./Title/Title";
 export { default as SubTitle } from "./SubTitle/SubTitle";
 export { default as Button } from "./Button/Button";
-export { default as Navbar } from "./Navbar/Navbar";
+export { default as Navbar } from "./NavBar/Navbar";
 export { default as HeadContainer } from "./HeadContainer/HeadContainer";
 export { default as Card } from "./Card/Card";
 export { default as Paragraph } from "./Paragraph/Paragraph";
